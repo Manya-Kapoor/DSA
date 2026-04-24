e417bb9026f237934b27db829985a4e577bf74d2
Manya Kapoor <blessedmanyakapoor1111@gmail.com>
2026-04-24 19:18:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        //  TWO POINTER
-        Arrays.sort(numbers);
-        int n = numbers.length;
-        int i = 0;
-        int j = n - 1;
-        while (i < j) {
-            int sum = numbers[i] + numbers[j];
-            if(sum == target) {
-                return new int[] {i+1, j+1 }; // +1 as problem requires 1-based indexing.
+        // HASHMAP
+        HashMap<Integer, Integer> map = new HashMap<>(); // to store each number and its index 
+        int res[] = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            int complement = target - numbers[i];
+            // Check if this complement exists in the hash map. If it does, then the current number and the number corresponding to the complement form the required pair.
+            if (map.containsKey(complement)) {
+                res[0] = map.get(complement) + 1;
+                res[1] = i + 1;
+                return res;
             }
-            else if (sum < target) i++;
-            else j--;
+            map.put(numbers[i], i); //  If the complement is not found, add the current number and its index to the hash map and continue.
         }
-        return new int[] {-1, -1};
+        return new int[] { -1, -1 };
     }
 }
-
-// TC = O(nlogn)
-// SC = O(1)
+// Time complexity: O(n)-  Each element is processed once, and hash map operations (insertion and lookup) are O(1) on average.
+// Space complexity: O(n) -  In the worst case, all n elements are stored in the hash map.
