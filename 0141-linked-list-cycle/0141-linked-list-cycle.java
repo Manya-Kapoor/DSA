f40cc0c548f13105ca6c1ebe655c83492be2d4dd
Manya Kapoor <blessedmanyakapoor1111@gmail.com>
2026-06-16 09:56:23 +0530

    Time: 1 ms (13.32%), Space: 46.9 MB (15.75%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,34 +9,22 @@
  *     }
  * }
  */
+
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // Hash Table Method
 
-        Set<ListNode> visited_nodes = new HashSet<>();
-        ListNode currNode = head;
-
-        while (currNode != null) {
-            if (visited_nodes.contains(currNode)) {
-                return true;
-            }
-            visited_nodes.add(currNode);
-            currNode = currNode.next;
+        // Value manipulation
+        
+        while (head != null) {
+            if (head.val == 1_000_000) return true;
+            head.val = 1_000_000;
+            head = head.next;
         }
         return false;
     }
 }
 
-/* Intuition and Logic Behind the Solution
-The basic idea here is to traverse the list and keep a record of the nodes we've visited so far. If at any point we encounter a node that we've already visited, we can conclude that there is a cycle in the list.
-
-Initialization:
-
-* Create an empty set, visited_nodes, to keep track of the nodes that have been visited.
-
-Traversal and Verification:
-
-* Traverse the list starting from the head node.
-* At each node, check whether it already exists in visited_nodes.
-* If it does, return True as a cycle is detected.
-* Otherwise, add the node to visited_nodes. */
+//since constraints for value are -10^5 <= Node.val <= 10^5
+//then I use 1_000_000 as indicator that this node has been
+//visited
